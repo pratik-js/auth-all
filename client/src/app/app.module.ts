4960cda9bb7884449e81edc53230a7a64ec61c52
Pratik <pratik-js@github.com>
2018-12-16 22:48:11 +0530

    pwa output

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -19,7 +19,9 @@ import { environment } from '../environments/environment';
     MaterialInUseModule,
     PagesModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
